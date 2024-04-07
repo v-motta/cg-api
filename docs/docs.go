363caed0dabfb39a431c63cda028661eb5b07639
v-motta <viniciusmotta0806@gmail.com>
2024-04-07 13:08:04 -0300

    Update API endpoint in Swagger configuration

--- docs/docs.go
+++ docs/docs.go
@@ -398,7 +398,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:9000",
+	Host:             "api.cost.vmotta.dev",
 	BasePath:         "/api/v1",
 	Schemes:          []string{"http"},
 	Title:            "Cost Guardian API",
