4b046c077fa6925740de005480499458649f0a26
v-motta <viniciusmotta0806@gmail.com>
2024-04-11 22:50:39 -0300

    Refactor SendEmail function in handlers/mail.go to include HTML email template

--- handlers/mail.go
+++ handlers/mail.go
@@ -28,7 +28,44 @@ func SendEmail(c echo.Context) error {
 	smtpHost := "smtp.gmail.com"
 	smtpPort := "587"
 
-	message := []byte("Subject: Cost Guardian\n\n" + mail.Message)
+	// message := []byte("Subject: Cost Guardian\n\n" + mail.Message)
+
+	subject := "Redefinição de senha"
+	body := `<!DOCTYPE html>
+	<html lang="pt">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Redefinição de Senha</title>
+			<style>
+				a {
+					padding: 15px;
+					background-color: #002952;
+					border-radius: 5px;
+					color: white;
+					text-decoration: none;
+				}
+				p {
+					color: #484848;
+				}
+			</style>
+		</head>
+		<body style="font-family: Roboto, sans-serif; background-color: #cfcfcf; padding: 20px;">
+			<table cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px; margin: auto; background-color: #fff; border-radius: 5px; overflow: hidden; box-shadow: 0 0 10px rgba(0,0,0,0.4);">
+				<tr>
+					<td style="padding: 20px;">
+						<p>Oi, [username]:</p>
+						<p style="padding-bottom: 20px">Você solicitou uma redefinição de senha para sua conta. Clique no botão abaixo para redefinir sua senha:</p>
+						<a href="[Inserir Link de Redefinição]">Redefinir senha</a>
+						<p style="padding-top: 20px">Se você não solicitou isso, pode ignorar este e-mail com segurança.</p>
+					</td>
+				</tr>
+			</table>
+		</body>
+	</html>`
+
+	message := []byte(fmt.Sprintf("From: %s\r\nTo: %s\r\nSubject: %s\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n%s",
+		from, to, subject, body))
 
 	auth := smtp.PlainAuth("", from, password, smtpHost)
 
@@ -38,5 +75,5 @@ func SendEmail(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to send email")
 	}
 
-	return echo.NewHTTPError(http.StatusOK, "Email Sent Successfully!")
+	return echo.NewHTTPError(http.StatusOK, "Email sent successfully!")
 }
